d51f3ead3776abd45cb801a11d62b65155991117
salah mbarki <salahmba004@gmail.com>
2024-02-26 17:01:28 +0100

    add redux toolkit

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,11 +1,14 @@
-import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
+import  {useDispatch ,useSelector} from 'react-redux';
+import {signInStart , signInFailure , singnInSuccess} from '../redux/user/userSlice.js';
+import { useState } from "react";
 
 export default function SignIn() {
   const [formData, setformData] = useState({});
-  const [error, seterror] = useState(null);
-  const [loading, setloading] = useState(false);
+  const {loading ,error} = useSelector((state) => state.user);
   const navigate = useNavigate();
+  const dispatch = useDispatch();
+
 
   const handleChange = (e) => {
     setformData({
@@ -16,7 +19,7 @@ export default function SignIn() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      setloading(true);
+      dispatch(signInStart())
       const res = await fetch("/api/auth/signin", {
         method: "POST",
         headers: {
@@ -27,16 +30,13 @@ export default function SignIn() {
       const data = await res.json();
       console.log(data);
       if (data.success === false) {
-        setloading(false);
-        seterror(data.message);
+        dispatch(signInFailure(data.message));
         return;
       }
-      setloading(false);
-      seterror(null);
+      dispatch(singnInSuccess(data));
       navigate("/");
     } catch (error) {
-      setloading(false);
-      seterror(error.message);
+      dispatch(signInFailure(data.message));
     }
   };
   console.log(formData);
